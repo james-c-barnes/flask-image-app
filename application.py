17e8af0cdc290572d6fd3a25223dc57dd9ae75e5
james-c-barnes <james_c_barnes@yahoo.com>
2016-12-16 01:02:13 +0000

    Updated secret key -- required.

--- application.py
+++ application.py
@@ -19,6 +19,7 @@ application = Flask(__name__)
 application.debug=True
 # change this to your own value
 # application.secret_key = 'cC1YCIWOj9GgWspgNEo2'   
+application.secret_key = 'GDxTkzGIWeDekQYm42Lg'
 
 # create an API for this application
 api = Api(application)
